f1f85d10f206635b7de585324fbf3f1313062419
Axel Sjöholm <Axel.sjoholm@elev.ga.ntig.se>
2025-04-11 00:52:04 +0200

    Uppdaterat funktion I clientmain för en mer användarvänlig meny

--- src/ClientMain.java
+++ src/ClientMain.java
@@ -10,51 +10,49 @@ class ClientMain {
         ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
         ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
         System.out.println("Welcome to TopShop");
-        int choice;
+        int choice = 0;
+        outer:
         while(true) {
             System.out.println("1. Login\n2. Create Account");
             try{
                 choice = input.nextInt();
-                if(choice != 1 || choice != 2){
-                    System.out.println("Not a choice");
-                } else {
-                    break;
-                }
             } catch (Exception e) {
                 System.out.println("Not a choice");
+                continue;
             }
-        }
-        if(choice == 1){
-            while(true){
-                input.nextLine();
-                System.out.println("Username:");
-                String username = input.nextLine();
-                System.out.println("Password:");
-                String password = input.nextLine();
-                out.writeObject("1" + username + "," + password);
-                user = (User) in.readObject();
-                if (user == null){
-                    System.out.println("Wrong password or username");
-                    continue;
-                } else {
-                    user.showMenu();
+            if(choice == 1){
+                while(true){
+                    input.nextLine();
+                    System.out.println("Username:");
+                    String username = input.nextLine();
+                    System.out.println("Password:");
+                    String password = input.nextLine();
+                    out.writeObject("1" + username + "," + password);
+                    user = (User) in.readObject();
+                    if (user == null){
+                        System.out.println("Wrong password or username");
+                    } else {
+                        user.showMenu();
+                    }
                 }
-            }
-        } else if(choice == 2){
-            while(true){
-                System.out.println("Name:");
-                String name = input.nextLine();
-                System.out.println("Email:");
-                String email = input.nextLine();
-                System.out.println("IDnr:");
-                String idnr = input.nextLine();
-                System.out.println("Username:");
-                String username = input.nextLine();
-                System.out.println("Password:");
-                String password = input.nextLine();
-                out.writeObject("2" + name + "," + email + "," + idnr + "," + username + "," + password);
-                System.out.println("Kontot skapat");
-                break;
+            } else if(choice == 2){
+                while(true){
+                    System.out.println("Name:");
+                    String name = input.nextLine();
+                    System.out.println("Email:");
+                    String email = input.nextLine();
+                    System.out.println("IDnr:");
+                    String idnr = input.nextLine();
+                    System.out.println("Username:");
+                    String username = input.nextLine();
+                    System.out.println("Password:");
+                    String password = input.nextLine();
+                    out.writeObject("2" + name + "," + email + "," + idnr + "," + username + "," + password);
+                    System.out.println("Account created.\nReturning to main menu...");
+                    break;
+                }
+            } else{
+                System.out.println("Not a choice");
             }
         }
     }
